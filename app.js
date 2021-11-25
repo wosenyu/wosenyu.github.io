9202376b734d7aa017f7021bffd062da936147b0
Wosen <wosen.yu72@gmail.com>
2021-11-24 20:50:38 -0500

    jokes

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// const jokes = document.querySelector('#jokes');
+// const button = document.querySelector('button');
+
+// const addNewJoke = async () => {
+//     const jokeText = await getDadJoke();
+//     const newLI = document.createElement('LI');
+//     newLI.append(jokeText);
+//     jokes.append(newLI)
+// }
+
+// const getDadJoke = async () => {
+//     try {
+//         const config = { headers: { Accept: 'application/json' } }
+//         const res = await axios.get('https://catfact.ninja/fact?max_length=140', config)
+//         return res.data.fact;
+//     } catch (e) {
+//         return "NO JOKES AVAILABLE! SORRY :("
+//     }
+// }
+
+// button.addEventListener('click', addNewJoke)
+
 const jokes = document.querySelector('#jokes');
 const button = document.querySelector('button');
 
@@ -11,11 +33,12 @@ const addNewJoke = async () => {
 const getDadJoke = async () => {
     try {
         const config = { headers: { Accept: 'application/json' } }
-        const res = await axios.get('https://catfact.ninja/fact?max_length=140', config)
-        return res.data.fact;
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
     } catch (e) {
         return "NO JOKES AVAILABLE! SORRY :("
     }
+
 }
 
 button.addEventListener('click', addNewJoke)
